78de8b3212c2eed0f8420cb56898de8ec26c132f
pyanton <pyanton112@gmail.com>
2023-07-22 10:42:57 +0500

    final

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://192.168.0.15:3306/kata";
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/testHibernate";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
     private static Connection connection;
 
     static {
